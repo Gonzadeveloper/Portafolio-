67c83206f41fc70cb4769780c2e1864b3da6207d
gonzadeveloper <gonzaloncayssials@gmail.com>
2024-07-01 22:19:08 -0300

    faltana detalles

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
             <div className="card mb-3">
         <img src="https://d2a5isokysfowx.cloudfront.net/wp-content/uploads/2022/08/que-es-un-desarrollador-web.png" class="card-img-top" alt="..."/>
         <div className="card-body">
-          <p className="card-text p1"> Hola mi nombre es Gonzalo, soy un desarrollador web full stack y aqui vas a encontrar todos mis proyectos. Puedes cliquear en ellos para ver mas informacion tecnica de los mismos</p>
+          <p className="card-text p1"> ¡Hola soy <span>Gonzalo</span>! un <span>desarrollador web full stack</span> aquí vas a encontrar todos mis proyectos. Puedes cliquear en ellos para ver más informacion tecnica de los mismos y <span>Tecnologías</span> con las que fueron realizados</p>
           <a href="https://github.com/Gonzadeveloper" className="btn btn-light IconGA">Mi Github <img src={IconGitHub} className="icono-btn" /></a>
           <a href="https://ar.linkedin.com/in/gonzalo-cayssials-610bb5254" className="btn btn-light IconL">Mi Linkedin <img src={IconLinkeind} className="icono-btn" /></a>
         </div>
