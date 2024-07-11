2760bd85f0148b1fb8e6ba65d77b9650300bb1aa
gonzadeveloper <gonzaloncayssials@gmail.com>
2024-07-10 22:02:33 -0300

    cambio de cv

--- vite-project/src/Componets/Home/Home.jsx
+++ vite-project/src/Componets/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Home.css'; // Asegúrate de importar correctamente tu archivo CSS
 import avatar from "../../assets/avatar.jpg"
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa el CSS de Bootstrap
 
 function Home() {
   const textos = ["Gonzalo", "Desarrollador web full stack"];
@@ -22,34 +23,30 @@ function Home() {
   };
 
   const handleDownloadClick = () => {
-    // Aquí puedes definir la lógica para el botón de descarga del CV
-    // Por ejemplo, puedes redirigir a una URL específica para la descarga
-    window.location.href = 'https://drive.google.com/file/d/1aHPrHLnMbQ7NsO4XUYshH0W2L9W_drHa/view?usp=drive_link';
+    window.location.href = 'https://drive.google.com/file/d/1B3eKiIC0qj8gcfrZ_eVrZwERKY16-Rle/view?usp=drive_link';
   };
 
   return (
-    <div className='container containerHome' id='Home'>
-      <div className='div1'>
-        <div className="texto-animated">
-          <span className="texto-cambiar1">Hola, soy <br /></span>
-          <span className="texto-cambiar">{textos[indice]}</span>
-        </div>
-        <div>
-          <p className='TextoHomep'>Apasionado por la tecnologia, la musica, los videojuegos y las motos.</p>
+    <div className='container-fluid containerHome' id='Home'>
+      <div className='row'>
+        <div className='col-md-6'>
+          <div className="texto-animated">
+            <span className="texto-cambiar1">Hola, soy <br /></span>
+            <span className="texto-cambiar">{textos[indice]}</span>
+          </div>
+          <div>
+            <p className='TextoHomep'>Apasionado por la tecnología, la música, los videojuegos y las motos.</p>
 
-          
-           
-          {/* Botón de contacto */}
-          <button className="botonesHome1" onClick={() => scrollToContact()}><span>Contactame</span></button>
-
+            {/* Botón de contacto */}
+            <button className="botonesHome1" onClick={() => scrollToContact()}><span>Contactame</span></button>
 
             {/* Segundo botón */}
             <button className='botonesHome2' onClick={handleDownloadClick}><span>Descarga mi CV</span></button>
-
+          </div>
         </div>
-      </div>
-      <div className='div2'>
-        <img src={avatar} alt="" />
+        <div className='col-md-6'>
+          <img src={avatar} alt="" className="img-fluid" />
+        </div>
       </div>
     </div>
   );
