aa90a86e6bf905ddb45734b4f1b67ad01acbd350
Gonzadevolper <gonza.cay@gmail.com>
2024-10-23 23:30:57 -0300

    cambio de nombre boton descarga mi CV a mira mi CV

--- vite-project/src/Componets/Home/Home.jsx
+++ vite-project/src/Componets/Home/Home.jsx
@@ -51,7 +51,7 @@ function Home() {
           <div>
             <p className='TextoHomep'>Apasionado por la tecnología, la música, los videojuegos y las motos.</p>
             <button className="botonesHome1" onClick={scrollToContact}><span>Contactame</span></button>
-            <button className='botonesHome2' onClick={handleDownloadClick}><span>Descarga mi CV</span></button>
+            <button className='botonesHome2' onClick={handleDownloadClick}><span>Mira mi CV</span></button>
           </div>
         </div>
         <div className='divImg col-12 col-md-6'>
